244b3bd8f1e3f729a426482e4138e91109d13611
kiwi <daydayup1995@gmail.com>
2022-07-11 13:22:57 -0500

    final

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,9 @@ var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+var languageButtonsEl = document.querySelector('#language-buttons');
+
+
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -59,8 +62,10 @@ var displayRepos = function(repos, searchTerm) {
     var repoName = repos[i].owner.login + '/' + repos[i].name;
 
     // create a container for each repo
-    var repoEl = document.createElement('div');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
+   var repoEl = document.createElement("a");
+  repoEl.classList = "list-item flex-row justify-space-between align-center";
+  repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+
 
     // create a span element to hold repository name
     var titleEl = document.createElement('span');
@@ -91,3 +96,29 @@ var displayRepos = function(repos, searchTerm) {
 
 // add event listeners to forms
 userFormEl.addEventListener('submit', formSubmitHandler);
+
+var getFeaturedRepos = function(language){
+  var apiUrl = "http://api.github.com/search/repositories?q="+language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response){
+    if(response.ok){
+      response.json().then(function(data){
+     displayRepos(data.items, language)
+      });
+    }else{
+      alert("Error: Github user not found")
+    }
+  })
+};
+// getFeaturedRepos("javascript")
+
+var buttonClickHander = function(event){
+var language = event.target.getAttribute("data-language");
+
+if(language){
+  getFeaturedRepos(language);
+  repoContainerEl.textContent = " ";
+}
+}
+
+languageButtonsEl.addEventListener("click",buttonClickHander)
